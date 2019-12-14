905e6c9390cdb88beacff33f153e2aef7dd9d584
Dan Gastler <dgastler@gmail.com>
2019-12-14 14:27:58 +0100

    gcc seems to build

--- mods/build_gcc.sh
+++ mods/build_gcc.sh
@@ -12,126 +12,127 @@ mkdir -p ${INSTALL_PATH}
 source /opt/Xilinx/petalinux/2018.2/settings.sh
 
 
-#####cd ${BUILD_PATH}
-#####mkdir binutils
-#####cd binutils
-#####wget http://ftpmirror.gnu.org/binutils/binutils-2.27.tar.gz
-#####tar xf binutils-2.27.tar.gz
-#####mkdir build && cd build
-#####../binutils-2.27/configure \
-#####    CC=arm-linux-gnueabihf-gcc \
-#####    CXX=arm-linux-gnueabihf-g++ \
-#####    AR=arm-linux-gnueabihf-ar \
-#####    RANLIB=arm-linux-gnueabihf-ranlib \
-#####    LD=arm-linux-gnueabihf-ld \
-#####    NM=arm-linux-gnueabihf-gcc-nm \
-#####    --host=arm-linux-gnueabihf \
-#####    --build=x86_64-linux-gnu \
-#####    --prefix=${INSTALL_PATH} \
-#####    --with-arch=armv7 \
-#####    --with-fpu=vfp \
-#####    --with-float=hard \
-#####    --disable-ultilib \
-#####    --with-no-thumb-interwork \
-#####    --with-mode=thumb
-#####make -j8 all install
-#####export PATH="$PATH:$INSTALL_PATH/arm-none-eabi/bin"
-#####
-#####cd ${BUILD_PATH}
-#####mkdir gmp
-#####cd gmp
-#####wget ftp://gcc.gnu.org/pub/gcc/infrastructure/gmp-6.1.0.tar.bz2
-#####tar xf gmp-6.1.0.tar.bz2
-#####mkdir build && cd build
-#####../gmp-6.1.0/configure \
-#####    CC=arm-linux-gnueabihf-gcc \
-#####    CXX=arm-linux-gnueabihf-g++ \
-#####    AR=arm-linux-gnueabihf-ar \
-#####    RANLIB=arm-linux-gnueabihf-ranlib \
-#####    LD=arm-linux-gnueabihf-ld \
-#####    NM=arm-linux-gnueabihf-gcc-nm \
-#####    CFLAGS="-I. -I./Include -IInclude -I${INSTALL_PATH}/include -I${SYSROOT}/usr/include" \
-#####    LDFLAGS="--sysroot=${SYSROOT} -L${PWD}/image/lib -L${INSTALL_PATH}/lib -L${SYSROOT}/usr/lib" \
-#####    --host=arm-linux-gnueabihf \
-#####    --build=x86_64-linux-gnu \
-#####    --prefix=${INSTALL_PATH} \
-#####    --with-arch=armv7 \
-#####    --with-fpu=vfp \
-#####    --with-float=hard \
-#####    --disable-ultilib \
-#####    --with-no-thumb-interwork \
-#####    --with-mode=thumb
-#####make -j8 all 
-#####make install
-#####
-#####
-#####cd ${BUILD_PATH}
-#####mkdir mpfr
-#####cd mpfr
-#####wget ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-3.1.4.tar.bz2
-#####tar xf mpfr-3.1.4.tar.bz2
-#####mkdir build && cd build
-#####../mpfr-3.1.4/configure \
-#####    CC=arm-linux-gnueabihf-gcc \
-#####    CXX=arm-linux-gnueabihf-g++ \
-#####    AR=arm-linux-gnueabihf-ar \
-#####    RANLIB=arm-linux-gnueabihf-ranlib \
-#####    LD=arm-linux-gnueabihf-ld \
-#####    NM=arm-linux-gnueabihf-gcc-nm \
-#####    CFLAGS="-I. -I./Include -IInclude -I${INSTALL_PATH}/include -I${SYSROOT}/usr/include" \
-#####    LDFLAGS="--sysroot=${SYSROOT} -L${PWD}/image/lib -L${INSTALL_PATH}/lib -L${SYSROOT}/usr/lib" \
-#####    --host=arm-linux-gnueabihf \
-#####    --build=x86_64-linux-gnu \
-#####    --prefix=${INSTALL_PATH} \
-#####    --with-arch=armv7 \
-#####    --with-fpu=vfp \
-#####    --with-float=hard \
-#####    --disable-ultilib \
-#####    --with-no-thumb-interwork \
-#####    --with-mode=thumb
-#####make -j8 all 
-#####make install
-#####
-#####cd ${BUILD_PATH}
-#####mkdir mpc
-#####cd mpc
-#####wget ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-1.0.3.tar.gz
-#####tar xf mpc-1.0.3.tar.gz
-#####mkdir build && cd build
-#####../mpc-1.0.3/configure \
-#####    CC=arm-linux-gnueabihf-gcc \
-#####    CXX=arm-linux-gnueabihf-g++ \
-#####    AR=arm-linux-gnueabihf-ar \
-#####    RANLIB=arm-linux-gnueabihf-ranlib \
-#####    LD=arm-linux-gnueabihf-ld \
-#####    NM=arm-linux-gnueabihf-gcc-nm \
-#####    CFLAGS="-I. -I./Include -IInclude -I${INSTALL_PATH}/include -I${SYSROOT}/usr/include" \
-#####    LDFLAGS="--sysroot=${SYSROOT} -L${PWD}/image/lib -L${INSTALL_PATH}/lib -L${SYSROOT}/usr/lib" \
-#####    --host=arm-linux-gnueabihf \
-#####    --build=x86_64-linux-gnu \
-#####    --prefix=${INSTALL_PATH} \
-#####    --with-arch=armv7 \
-#####    --with-fpu=vfp \
-#####    --with-float=hard \
-#####    --disable-ultilib \
-#####    --with-no-thumb-interwork \
-#####    --with-mode=thumb
-#####make -j8 all 
-#####make install
-#####
+######cd ${BUILD_PATH}
+######mkdir binutils
+######cd binutils
+######wget http://ftpmirror.gnu.org/binutils/binutils-2.27.tar.gz
+######tar xf binutils-2.27.tar.gz
+######mkdir build && cd build
+######../binutils-2.27/configure \
+######    CC=arm-linux-gnueabihf-gcc \
+######    CXX=arm-linux-gnueabihf-g++ \
+######    AR=arm-linux-gnueabihf-ar \
+######    RANLIB=arm-linux-gnueabihf-ranlib \
+######    LD=arm-linux-gnueabihf-ld \
+######    NM=arm-linux-gnueabihf-gcc-nm \
+######    --host=arm-linux-gnueabihf \
+######    --build=x86_64-linux-gnu \
+######    --prefix=${INSTALL_PATH} \
+######    --with-arch=armv7 \
+######    --with-fpu=vfp \
+######    --with-float=hard \
+######    --disable-ultilib \
+######    --with-no-thumb-interwork \
+######    --with-mode=thumb
+######make -j8 all install
+######export PATH="$PATH:$INSTALL_PATH/arm-none-eabi/bin"
+######
+######cd ${BUILD_PATH}
+######mkdir gmp
+######cd gmp
+######wget ftp://gcc.gnu.org/pub/gcc/infrastructure/gmp-6.1.0.tar.bz2
+######tar xf gmp-6.1.0.tar.bz2
+######mkdir build && cd build
+######../gmp-6.1.0/configure \
+######    CC=arm-linux-gnueabihf-gcc \
+######    CXX=arm-linux-gnueabihf-g++ \
+######    AR=arm-linux-gnueabihf-ar \
+######    RANLIB=arm-linux-gnueabihf-ranlib \
+######    LD=arm-linux-gnueabihf-ld \
+######    NM=arm-linux-gnueabihf-gcc-nm \
+######    CFLAGS="-I. -I./Include -IInclude -I${INSTALL_PATH}/include -I${SYSROOT}/usr/include" \
+######    LDFLAGS="--sysroot=${SYSROOT} -L${PWD}/image/lib -L${INSTALL_PATH}/lib -L${SYSROOT}/usr/lib" \
+######    --host=arm-linux-gnueabihf \
+######    --build=x86_64-linux-gnu \
+######    --prefix=${INSTALL_PATH} \
+######    --with-arch=armv7 \
+######    --with-fpu=vfp \
+######    --with-float=hard \
+######    --disable-ultilib \
+######    --with-no-thumb-interwork \
+######    --with-mode=thumb
+######make -j8 all 
+######make install
+######
+######
+######cd ${BUILD_PATH}
+######mkdir mpfr
+######cd mpfr
+######wget ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-3.1.4.tar.bz2
+######tar xf mpfr-3.1.4.tar.bz2
+######mkdir build && cd build
+######../mpfr-3.1.4/configure \
+######    CC=arm-linux-gnueabihf-gcc \
+######    CXX=arm-linux-gnueabihf-g++ \
+######    AR=arm-linux-gnueabihf-ar \
+######    RANLIB=arm-linux-gnueabihf-ranlib \
+######    LD=arm-linux-gnueabihf-ld \
+######    NM=arm-linux-gnueabihf-gcc-nm \
+######    CFLAGS="-I. -I./Include -IInclude -I${INSTALL_PATH}/include -I${SYSROOT}/usr/include" \
+######    LDFLAGS="--sysroot=${SYSROOT} -L${PWD}/image/lib -L${INSTALL_PATH}/lib -L${SYSROOT}/usr/lib" \
+######    --host=arm-linux-gnueabihf \
+######    --build=x86_64-linux-gnu \
+######    --prefix=${INSTALL_PATH} \
+######    --with-arch=armv7 \
+######    --with-fpu=vfp \
+######    --with-float=hard \
+######    --disable-ultilib \
+######    --with-no-thumb-interwork \
+######    --with-mode=thumb
+######make -j8 all 
+######make install
+######
+######cd ${BUILD_PATH}
+######mkdir mpc
+######cd mpc
+######wget ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-1.0.3.tar.gz
+######tar xf mpc-1.0.3.tar.gz
+######mkdir build && cd build
+######../mpc-1.0.3/configure \
+######    CC=arm-linux-gnueabihf-gcc \
+######    CXX=arm-linux-gnueabihf-g++ \
+######    AR=arm-linux-gnueabihf-ar \
+######    RANLIB=arm-linux-gnueabihf-ranlib \
+######    LD=arm-linux-gnueabihf-ld \
+######    NM=arm-linux-gnueabihf-gcc-nm \
+######    CFLAGS="-I. -I./Include -IInclude -I${INSTALL_PATH}/include -I${SYSROOT}/usr/include" \
+######    LDFLAGS="--sysroot=${SYSROOT} -L${PWD}/image/lib -L${INSTALL_PATH}/lib -L${SYSROOT}/usr/lib" \
+######    --host=arm-linux-gnueabihf \
+######    --build=x86_64-linux-gnu \
+######    --prefix=${INSTALL_PATH} \
+######    --with-arch=armv7 \
+######    --with-fpu=vfp \
+######    --with-float=hard \
+######    --disable-ultilib \
+######    --with-no-thumb-interwork \
+######    --with-mode=thumb
+######make -j8 all 
+######make install
+
 
 
 cd ${BUILD_PATH}
-mkdir gcc-4.7.1
-cd gcc-4.7.1
+mkdir gcc
+cd gcc
+cp ../../mods/patch-gcc_cp_cfns.h ./
 wget ftp://ftp.gnu.org/gnu/gcc/gcc-4.7.1/gcc-4.7.1.tar.bz2
 tar xf gcc-4.7.1.tar.bz2
 mkdir build && cd build
+patch -u -b ../gcc-4.7.1/gcc/cp/cfns.h -i ../patch-gcc_cp_cfns.h
 ../gcc-4.7.1/configure \
-    CFLAGS="-I. -I./Include -IInclude -I${INSTALL_PATH}/include -I${SYSROOT}/usr/include" \
-    LDFLAGS="--sysroot=${SYSROOT} -L${PWD}/image/lib -L${INSTALL_PATH}/lib -L${SYSROOT}/usr/lib -L${SYSROOT}/lib" \
-    --with-build-sysroot=${SYSROOT} \
-    --with-lib-path=${INSTALL_PATH}/lib:${INSTALL_PATH}/usr/lib:${SYSROOT}/lib:${SYSROOT}/usr/lib \
+    --with-gmp=${INSTALL_PATH} \
+    --with-mpfr=${INSTALL_PATH} \
+    --with-mpc=${INSTALL_PATH} \
     --host=arm-linux-gnueabihf \
     --build=x86_64-linux-gnu \
     --prefix=${INSTALL_PATH} \
@@ -144,6 +145,14 @@ mkdir build && cd build
     --with-mode=thumb 
 make all-gcc 
 make install-gcc
+
+#    --with-build-sysroot=${SYSROOT} \
+#    --with-lib-path=${INSTALL_PATH}/lib:${INSTALL_PATH}/usr/lib:${SYSROOT}/lib:${SYSROOT}/usr/lib \
+#    --with-lib-path=${INSTALL_PATH}/lib:${INSTALL_PATH}/usr/lib:${SYSROOT}/lib:${SYSROOT}/usr/lib \
+
+#    CFLAGS="-I. -I./Include -IInclude -I${INSTALL_PATH}/include -I${SYSROOT}/usr/include" \
+#    LDFLAGS="--sysroot=${SYSROOT} -L${PWD}/image/lib -L${INSTALL_PATH}/lib -L${SYSROOT}/usr/lib -L${SYSROOT}/lib" \
+
 #    CC=arm-linux-gnueabihf-gcc \
 #    CXX=arm-linux-gnueabihf-g++ \
 #    AR=arm-linux-gnueabihf-ar \
