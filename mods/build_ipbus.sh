da8f8049ba189154ed6a9c522038867880486ca3
Dan Gastler <dgastler@gmail.com>
2019-12-19 01:45:58 +0100

    Fixed ipbus install

--- mods/build_ipbus.sh
+++ mods/build_ipbus.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /tmp/ipbus-software
 make -j32 Set=uhal BUILD_PUGIXML=0 BUILD_UHAL_TESTS=1 BUILD_UHAL_PYCOHAL=1
-make install
+make Set=uhal BUILD_PUGIXML=0 BUILD_UHAL_TESTS=1 BUILD_UHAL_PYCOHAL=1 install
