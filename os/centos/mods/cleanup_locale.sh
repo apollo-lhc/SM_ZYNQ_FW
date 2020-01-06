4cc654f5d933fda3be52864dc1fb179e7b02b2ee
Dan Gastler <dgastler@gmail.com>
2020-01-06 16:05:24 +0100

    Update to fix segfault in cleanup script

--- os/centos/mods/cleanup_locale.sh
+++ os/centos/mods/cleanup_locale.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 localedef --list-archive | grep -E -v "^en" | xargs localedef --delete-from-archive
-mv /usr/lib/locale/locale-archive /usr/lib/locale/locale-archive.tmpl
+cp /usr/lib/locale/locale-archive /usr/lib/locale/locale-archive.tmpl
 build-locale-archive 
 
 ls -p1d /usr/share/locale/*/  | grep -v "/en" | xargs rm -rf
