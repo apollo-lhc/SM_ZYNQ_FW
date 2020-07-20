62f601b6087a9fc2a826057052923f8851aad008
Dan Gastler <dgastler@gmail.com>
2020-07-20 18:49:07 -0400

    python can grab dtsi files

--- os/GetReleases.py
+++ os/GetReleases.py
@@ -5,11 +5,31 @@ import yaml
 import requests #interact with github.com
 import json # parse github return
 
+def GetReleaseFiles(host, project,repo, release):
+  GIT_API_URL="https://api."+host+"/repos/"+project+"/"+repo+"/releases"
+  response=requests.get(GIT_API_URL)
+  ReleaseJSON=json.loads(response.text)
+  for remoteRelease in ReleaseJSON:
+    if remoteRelease["name"] == release:
+      for asset in remoteRelease["assets"]:
+        if asset["name"].find("dtsi") != -1:
+          assetData = requests.get(asset["url"],headers = {"Accept": "application/octet-stream"})
+          filename="hw_remote/"+asset["name"].replace("dtsi.","")
+          open(filename,'wb').write(assetData.content)
+        
+
+
+
+
 CMFile=open("CM.yaml")
 
 remotes=yaml.load(CMFile)
-print remotes
+
 for remote in remotes:
-  print remote["repo"]
+  print remote
+  GetReleaseFiles(remotes[remote]['host'],
+                  remotes[remote]['project'],
+                  remotes[remote]['repo'],
+                  remotes[remote]['release'])
 
  
