9f390908fe198dc3b95aede1054ef35c52954508
Dan Gastler <dgastler@gmail.com>
2020-08-19 15:50:23 -0400

    Cleanup and using a new Apollo tool version

--- os/mk/BUTool.mk
+++ os/mk/BUTool.mk
@@ -1,19 +1,13 @@
 
 #Build BUTool from ApolloTool meta repo
-APOLLO_TOOL_TAG=v1.2.1
+APOLLO_TOOL_TAG=v1.3
 ${OPT_PATH}/BUTool: ${OPT_PATH}/cactus | ${OPT_PATH} ${TMP_PATH} 
 	cd ${TMP_PATH} && \
 		git clone --branch ${APOLLO_TOOL_TAG} https://github.com/apollo-lhc/ApolloTool.git
 	cd ${TMP_PATH}/ApolloTool && \
 		make init
-	cd ${TMP_PATH}/ApolloTool/BUTool && \
-		git checkout feature-IP_Status
-	cd ${TMP_PATH}/ApolloTool/plugins/ApolloSM_plugin && \
-		git checkout feature-xvc_info
 	cp ${MODS_PATH}/build_BUTool.sh ${TMP_PATH}/ApolloTool/
 	sudo chroot ${INSTALL_PATH} ${QEMU_PATH}/${QEMU} /bin/bash /tmp/ApolloTool/build_BUTool.sh
-#	sudo install -d -m 755 ${OPT_PATH}/address_table
-#	(cd mods && find address_tables/ -type f -exec sudo install -Dm 666 "{}" "${OPT_PATH}/{}" \;)
 	(find address_table/ -xtype f -exec sudo install -Dm 666 "{}" "${OPT_PATH}/{}" \;)
 	sudo ln -s /opt/address_table ${OPT_PATH}/address_tables
 	sudo install -d -m 755 ${OPT_PATH}/BUTool/systemd
