d6531c6beb7991bfd8a6505389e33c078751f3c3
Dan Gastler <dgastler@gmail.com>
2022-04-11 09:49:17 -0400

    Updates to makefiles and build scripts to keep in line with version settings in makefiles

--- kernel/scripts/build_container.sh
+++ kernel/scripts/build_container.sh
@@ -2,17 +2,19 @@
 
 KERNEL_BUILD_PATH=/app/kernel
 
-if [ $# -gt 0 ]; then
+if [ $# -gt 2 ]; then
     BUILD_NAME=$1
     #add the packages we need
-    yum -y install iproute gcc gcc-c++ net-tools ncurses-devel zlib-devel openssl-devel flex bison libselinux xterm autoconf libtool texinfo SDL-devel glibc-devel glibc glib2-devel automake screen pax libstdc++ gawk python3 python3-pip python3-GitPython python3-jinja2 perl patch diffutils cpp perl-Data-Dumper perl-Text-ParseWords per-Thread-Queue xz which make rsync file sudo
+    yum -y install iproute gcc gcc-c++ net-tools ncurses-devel zlib-devel openssl-devel flex bison libselinux xterm autoconf libtool texinfo SDL-devel glibc-devel glibc glib2-devel automake screen pax libstdc++ gawk python3 python3-pip python3-GitPython python3-jinja2 perl patch diffutils cpp perl-Data-Dumper perl-Text-ParseWords perl-Thread-Queue xz which make rsync file sudo
 
     #petalinux won't build as root(the defaul docker user) so make a test user
     adduser test
 
     cd ${KERNEL_BUILD_PATH}
+
+        
     sudo -u test make clean
-    sudo -u test make ${BUILD_NAME}
+    sudo -u test BUILD_PETALINUX_VERSION=$2 BUILD_PETALINUX_ROOT=$3 make ${BUILD_NAME}
 else
     echo "Missing build name.  ex. rev2a_xczu7ev"
 fi
