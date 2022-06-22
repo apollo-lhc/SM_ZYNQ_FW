1875891f35700e1d67b212bbe7803713bf70669d
Dan Gastler <dgastler@gmail.com>
2022-06-22 11:13:15 -0400

    Updated rev1 45 build files

--- configs/rev1_xc7z045/files.tcl
+++ configs/rev1_xc7z045/files.tcl
@@ -16,6 +16,7 @@ set vhdl_files "\
      src/misc/pacd.vhd \
      src/misc/capture_CDC.vhd \
      src/misc/rate_counter.vhd \
+     src/misc/XILINX_DNA.vhd \
      regmap_helper/axiReg/axiRegWidthPkg_32.vhd \
      regmap_helper/axiReg/axiRegPkg.vhd \
      regmap_helper/axiReg/axiReg.vhd \
@@ -24,28 +25,33 @@ set vhdl_files "\
      regmap_helper/axiReg/axiRegBlocking.vhd \
      src/services/services_rev1.vhd \
      src/services/SGMII_MON_pkg.vhd \
-     configs/rev1_xc7z045/autogen/services/SERV_map.vhd \
-     configs/rev1_xc7z045/autogen/services/SERV_PKG.vhd \
+     ${autogen_path}/SERV/SERV_map.vhd \
+     ${autogen_path}/SERV/SERV_PKG.vhd \
      src/IPMC_i2c_slave/i2c_slave.vhd \
      src/IPMC_i2c_slave/IPMC_i2c_slave.vhd \
-     configs/rev1_xc7z045/autogen/CM_interface/CM_map.vhd \
-     configs/rev1_xc7z045/autogen/CM_interface/CM_PKG.vhd \
+     ${autogen_path}/CM/CM_map.vhd \
+     ${autogen_path}/CM/CM_PKG.vhd \
      src/CM_interface/CM_interface.vhd \
      src/CM_interface/CM_Monitoring.vhd \
      src/CM_interface/CM_pwr.vhd \
      src/CM_interface/CM_package.vhd \
      src/CM_interface/CM_phy_lane_control.vhd \
+     src/CM_interface/picoblaze/picoblaze/cli.vhd \
+     src/CM_interface/picoblaze/picoblaze/kcpsm6.vhd \
+     src/CM_interface/picoblaze/uart_rx6.vhd \
+     src/CM_interface/picoblaze/uart_tx6.vhd \
+     src/CM_interface/picoblaze/uC.vhd \    
      src/front_panel/Button_Debouncer.vhd \
      src/front_panel/Button_Decoder.vhd \
      src/front_panel/FrontPanel_UI.vhd \
      src/front_panel/LED_Encoder.vhd \
      src/front_panel/SR_Out.vhd \
      src/front_panel/LED_Paterns.vhd \
-     configs/rev1_xc7z045/autogen/SM_info/SM_INFO_map.vhd \
-     configs/rev1_xc7z045/autogen/SM_info/SM_INFO_PKG.vhd \
+     ${autogen_path}/SM_INFO/SM_INFO_map.vhd \
+     ${autogen_path}/SM_INFO/SM_INFO_PKG.vhd \
      src/SM_info/SM_info.vhd \
-     configs/rev1_xc7z045/autogen/plXVC/PLXVC_map.vhd \
-     configs/rev1_xc7z045/autogen/plXVC/PLXVC_PKG.vhd \
+     ${autogen_path}/PLXVC/PLXVC_map.vhd \
+     ${autogen_path}/PLXVC/PLXVC_PKG.vhd \
      src/plXVC/plXVC_intf.vhd \
      src/plXVC/virtualJTAG.vhd \
      "
@@ -54,7 +60,4 @@ set xdc_files "\
      configs/rev1_xc7z045/src/top.xdc \
      "
 
-set xci_files "\
-     configs/rev1_xc7z045/cores/onboardClk.tcl \
-    	      "
 
--- kernel/scripts/build_container.sh
+++ kernel/scripts/build_container.sh
@@ -3,9 +3,18 @@
 KERNEL_BUILD_PATH=/app/kernel
 
 if [ $# -gt 2 ]; then
+    unset LD_LIBRARY_PATH
     BUILD_NAME=$1
     #add the packages we need
-    yum -y install iproute gcc gcc-c++ net-tools ncurses-devel zlib-devel openssl-devel flex bison libselinux xterm autoconf libtool texinfo SDL-devel glibc-devel glibc glib2-devel automake screen pax libstdc++ gawk python3 python3-pip python3-GitPython python3-jinja2 perl patch diffutils cpp perl-Data-Dumper perl-Text-ParseWords perl-Thread-Queue xz which make rsync file sudo
+    yum -y install \
+	iproute gawk gcc gcc-c++ net-tools ncurses-devel \
+	zlib-devel openssl-devel flex bison libselinux \
+	xterm autoconf libtool texinfo SDL-devel glibc-devel \
+	glibc glib2-devel automake screen pax libstdc++ gawk \
+	python3 python3-pip python3-GitPython python3-jinja2 \
+	perl patch diffutils cpp perl-Data-Dumper \
+	perl-Text-ParseWords perl-Thread-Queue xz which make \
+	rsync file sudo patch gnupg2 less
 
     #petalinux won't build as root(the defaul docker user) so make a test user
     adduser test
