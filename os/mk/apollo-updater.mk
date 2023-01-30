65c0cc07e0ca926392c4df26edf5d812c7d316ed
Dan Gastler <dgastler@gmail.com>
2023-01-30 14:40:27 -0500

    Removed OS files.  They are now in a separate repo

--- os/mk/apollo-updater.mk
+++
@@ -1,9 +0,0 @@
-APOLLO_UPDATER_BRANCH=v1.0.0
-APOLLO_UPDATER_URI=https://gitlab.com/apollo-lhc/soc-tools/apollo-updater.git
-APOLLO_UPDATER_NAME=apollo-updater
-
-%/opt/${APOLLO_UPDATER_NAME}: OPT_PATH=$*/opt/
-
-%/opt/${APOLLO_UPDATER_NAME}: %/opt/
-	cd ${OPT_PATH} && \
-		git clone --branch ${APOLLO_UPDATER_BRANCH} ${APOLLO_UPDATER_URI} ${APOLLO_UPDATER_NAME}
